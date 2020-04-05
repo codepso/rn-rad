2fd07caae59a9ca1d1ed82f969799efef45ba65c
Juan Minaya <minayaleon@gmail.com>
2020-04-05 17:43:14 -0500

    Remove props for components

--- src/utils/prints.js
+++ src/utils/prints.js
@@ -10,7 +10,7 @@ const logo = () => {
 };
 
 const version = () => {
-  log(chalk.white('Version ') + chalk.yellow('1.0.8'));
+  log(chalk.white('Version ') + chalk.yellow('1.0.14'));
   log('');
 };
 
